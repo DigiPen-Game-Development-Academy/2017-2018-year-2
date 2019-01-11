8006bcecea2140e7110cb2e889f5b8ed581de26a
Myles-Busig <38596725+Myles-Busig@users.noreply.github.com>
2019-01-11 11:59:33 -0800

    Updated my fps and top down shooter game.

--- MylesBusig/Unity/Combat/Assets/Scripts/Player.cs
+++ MylesBusig/Unity/Combat/Assets/Scripts/Player.cs
@@ -1,6 +1,33 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum FireMode
+{
+	Cardinal,
+	Facing,
+	Mouse,
+	FacingT,
+	MouseT
+}
+public enum Direction
+{
+	F,
+	B,
+	FB,
+	L,
+	R,
+	LR,
+	A,
+	D
+}
+public enum CameraMode
+{
+	FirstPerson,
+	TopDown
+}
 
 [RequireComponent(typeof(Rigidbody))]
 public class Player : MonoBehaviour
@@ -9,9 +36,21 @@ public class Player : MonoBehaviour
 	public float walkSpeed = 3.0f;
 	public float runSpeed = 5.0f;
 	public float sensitivity = 7.0f;
+	public float projectileStartDistance = 1.0f;
+
 	public GameObject projectile = null;
 	public float maxFireRate = 0.2f;
 	float timeTillCanFire = 0.0f;
+
+	public FireMode fireMode = FireMode.Cardinal;
+	public Direction direction = Direction.F;
+	public CameraMode cameraMode = CameraMode.FirstPerson;
+	public Text fireModeTxt;
+	public Text directionTxt;
+	public Text cameraModeTxt;
+
+	new public GameObject camera;
+	public float cameraDistance = 4.0f;
 
 	new Rigidbody rigidbody;
 
@@ -42,18 +81,83 @@ public class Player : MonoBehaviour
 		if (Input.GetKey(KeyCode.D))
 			input.x += 1;
 
-		input = transform.TransformDirection(input);
-		Vector3 newVelocity = new Vector3(0.0f, rigidbody.velocity.y, 0.0f) + (input * currentSpeed);
+		if (Input.GetKeyDown(KeyCode.C))
+		{
+			if (cameraMode == CameraMode.FirstPerson)
+			{
+				cameraMode = CameraMode.TopDown;
+
+				camera.transform.SetParent(null);
+			}
+			else if (cameraMode == CameraMode.TopDown)
+			{
+				cameraMode = CameraMode.FirstPerson;
+
+				camera.transform.SetParent(transform);
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.F))
+		{
+			int i = (int)fireMode + 1;
+			if (Enum.IsDefined(typeof(FireMode), i))
+				fireMode = (FireMode)i;
+			else
+				fireMode = FireMode.Cardinal;
+		}
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			int i = (int)direction + 1;
+			if (Enum.IsDefined(typeof(Direction), i))
+				direction = (Direction)i;
+			else
+				direction = Direction.F;
+		}
+
+		Vector3 newVelocity = Vector3.zero;
+
+		if (cameraMode == CameraMode.FirstPerson)
+		{
+			camera.transform.localRotation = Quaternion.Euler(Vector3.zero);
+			camera.transform.localPosition = Vector3.zero;
+
+			input = transform.TransformDirection(input);
+			newVelocity = new Vector3(0.0f, rigidbody.velocity.y, 0.0f) + (input * currentSpeed);
+		}
+		else if (cameraMode == CameraMode.TopDown)
+		{
+			camera.transform.position = transform.position + Vector3.up * cameraDistance;
+			camera.transform.LookAt(transform.position);
+
+			newVelocity = input * currentSpeed;
+		}
+
 		rigidbody.velocity = Vector3.Lerp(rigidbody.velocity, newVelocity, accel * Time.deltaTime);
 
 		rigidbody.maxAngularVelocity = 0;
 		transform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * sensitivity);
-		
+
+		//Debug.Log("forward: " + transform.forward);
+
 		if (Input.GetMouseButton(0) && timeTillCanFire <= 0.0f)
 		{
-			GameObject newProjectile = Instantiate(projectile, transform.position + (transform.forward * 2), transform.rotation * Quaternion.Euler(90.0f, 0.0f, 0.0f));
+			//GameObject newProjectile = Instantiate(projectile, transform.position + (transform.forward * 2), transform.rotation * Quaternion.Euler(90.0f, 0.0f, 0.0f));
+			Shoot(transform.position, transform.forward);
 
 			timeTillCanFire = maxFireRate;
 		}
+
+		fireModeTxt.text = "FireMode: " + fireMode;
+		directionTxt.text = "Direction: " + direction;
+		cameraModeTxt.text = "CameraMode: " + cameraMode;
+	}
+
+	public GameObject Shoot(Vector3 position, Vector3 direction)
+	{
+		GameObject newProjectile = Instantiate(projectile, transform.position + (direction * projectileStartDistance), Quaternion.Euler(Vector3.Cross(Vector3.up, direction)) * Quaternion.Euler(90.0f, 0.0f, 0.0f));
+		
+		//Debug.Log("Rotation: " + (Quaternion.Euler(direction) * Quaternion.Euler(90.0f, 0.0f, 0.0f)));
+		Debug.Log("Direction: " + direction);
+
+		return newProjectile;
 	}
 }
--- MylesBusig/Unity/Combat/Assets/Scripts/Projectile.cs
+++ MylesBusig/Unity/Combat/Assets/Scripts/Projectile.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Projectile : MonoBehaviour
 {
+	public float speed = 2.0f;
+	public bool track = false;
+	public GameObject target;
+	public float despawnTime = 1.0f;
+
+	new Rigidbody rigidbody;
+
 	void Start()
 	{
-		
+		rigidbody = GetComponent<Rigidbody>();
 	}
 	
 	void Update()
 	{
-		
+		rigidbody.velocity = transform.up * speed;
+
+		Destroy(gameObject, despawnTime);
 	}
 }
